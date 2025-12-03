d7b355917a434db6bbebfcca4edd01c5159d61e5
John Lloyd Lomugdang <jlomugdang0728@gmail.com>
2025-12-04 01:14:24 +0800

    Initial commit for Vercel deployment

--- index.js
+++ index.js
@@ -118,7 +118,9 @@ app.use((err, req, res, next) => {
 // ===================================
 
 // Use PORT from .env or default 5000
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (require.main === module) {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
 module.exports = app;
